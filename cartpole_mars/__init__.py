d35816a0415d6538afbda307c26952c5d2e45e61
Sowmya Parthiban <sparthi1@jhu.edu>
2021-03-08 23:18:42 +0530

    update __init__.py

--- cartpole_mars/__init__.py
+++ cartpole_mars/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 
 register(
-    id='cartpole_mars_v0',
+    id='cartpolemars-v0',
     entry_point='cartpole_mars.envs:CartPoleEnv')
 
 # tags={'wrapper_config.TimeLimit.max_episode_steps': 500},
